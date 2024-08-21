0162ac66f7453c82e0af75aca2d8189ca01be9d8
winicon001 <winicon@live.com>
2024-08-21 21:00:12 +0100

    First Version of this package. Intended for collective command to a swarm of robots over serial comms to arduino and over direct ROS2 publisher/subscriber node comms from the dev machine to a robot running on Raspberry Pi

---
+++ build/robots_navigationController/share/robots_navigationController/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/winicon/RobotControl_ws/build/robots_navigationController"
---
+++ src/robots_nav_contr/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'robots_nav_contr'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='winicon',
+    maintainer_email='winicon@live.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "robots_nav_controller = robots_nav_contr.robotsnavcontroller:main",
+            "robot_command_receiver = robots_nav_contr.nav_commands_receiver:main",
+            "robots_serial_commands = robots_nav_contr.raspberry_arduinoSerial:main",
+        ],
+    },
+)
