2b1625952f82f947b26a5eb7d8355b29eeb16a5b
winicon001 <winicon@live.com>
2024-08-24 08:08:22 +0000

    added other modules for the robot functions

---
+++ src/robots_nav_contr/robots_nav_contr/ModulesTest.py
@@ -0,0 +1,11 @@
+import encoders
+import move
+import time
+import RPi.GPIO as GPIO
+import ultra
+
+while True:
+    move.setup()
+    move.move(100, 'forward', 'no', 1)
+    encoders.wheelEncodersReading()
+
--- src/robots_nav_contr/robots_nav_contr/redeye_nav_commands_receiver.py
+++ src/robots_nav_contr/robots_nav_contr/redeye_nav_commands_receiver.py
@@ -3,18 +3,50 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+import move
+import encoders
+import time
+import RPi.GPIO as GPIO
+import ultra
+
 # robots_nav_contr
 
 class AutoCommandReceiver(Node):
     def __init__(self):
         super().__init__("redeye_command_receiver")
-        self.get_logger().info("...Node initiated. Robot # Listening to robot_auto_command Node...")
+        self.get_logger().info("...Node initiated. Redeye Listening to robot_auto_command Node...")
         self.receiver_ = self.create_subscription(String, '/auto_command', self.receiver_callback, 10) # Message type to receive, name of the topic to subscribe to and the buffer size
 
         self.count = 0
 
     def receiver_callback(self, msg: String):
         self.get_logger().info(f"Received {msg.data}")
+        command = msg.data
+
+        match command:
+            case "hi":
+                print("Hello, How are you today ROS2?.")
+            case "Ready":
+                print("Cool.")
+            case "":
+                print("Say again Please")
+
+            case "start":
+                print("Okay, Program started")
+                move.setup()
+                move.move(100, 'forward', 'no', 1)
+                encoders.wheelEncodersReading()
+
+            case "stop":
+                # move.motorStop()
+                move.move(0, 'forward', 'no', 1)
+                print("Okay, Program stopped.")
+
+            case _:
+                print("The language doesn't matter; what matters is solving problems.")
+
+
+
 
 
 def main(args=None):
