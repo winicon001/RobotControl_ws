f2855c6c682272c4c454f7177647c4d5eba06a46
winicon001 <winicon@live.com>
2025-03-23 15:05:32 +0000

    Modified for Cotavia Subscriber noted

--- src/robots_nav_contr/setup.py
+++ src/robots_nav_contr/setup.py
@@ -25,6 +25,7 @@ setup(
             "robot_command_receiver = robots_nav_contr.nav_commands_receiver:main", # This is the robots command subscriber. Used on dev pc
             "robots_serial_commands = robots_nav_contr.raspberry_arduinoSerial:main", # This is the robots command subscriber. Used on smilebot
             "redeye_commands = robots_nav_contr.redeye_nav_commands_receiver:main", # This is the robots command subscriber. Used on redEye
+            "octavia_commands = robots_nav_contr.octavia_arduinoSerial:main", # This is the robots command subscriber. Used on Octavia
             
         ],
     },
